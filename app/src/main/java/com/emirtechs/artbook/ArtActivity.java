b06671bf41a5e9e8c8713fea0753d8d4135f086b
emiryucell <e.talhayucel@gmail.com>
2021-12-22 18:45:05 +0300

    Menu created.Art item added.

--- app/src/main/java/com/emirtechs/artbook/ArtActivity.java
+++ app/src/main/java/com/emirtechs/artbook/ArtActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.emirtechs.artbook.databinding.ActivityArtBinding;
+
 public class ArtActivity extends AppCompatActivity {
 
+    private ActivityArtBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art);
+        binding=ActivityArtBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
     }
     public void save(View view){
 
